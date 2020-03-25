2112de4d7841bfb7baf82bc5513b5f790c57728c
tudorgabriel24 <tudorgabriel1398@gmail.com>
2020-03-25 16:42:43 +0200

    lab6 v2

---
+++ Laboratoare/WCF si EF. Exemplu cu Post si Comment. - lab6/WFandEFLab/WFandEFLab/API/Comment.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Data.Entity;
+using WFandEFLab;
+namespace PostComment
+{
+    public partial class Comment
+    {
+        public bool AddComment()
+        {
+            using (ModelDesignContainer ctx = new ModelDesignContainer())
+            {
+                bool bResult = false;
+                if (this == null || this.postPostId == 0)
+                    return bResult;
+                if (this.commentId == 0)
+                {
+                    ctx.Entry<Comment>(this).State = EntityState.Added;
+                    Post p = ctx.Posts.Find(this.postPostId);
+                    ctx.Entry<Post>(p).State = EntityState.Unchanged;
+                    ctx.SaveChanges();
+                    bResult = true;
+                }
+                return bResult;
+            }
+        }
+        public Comment UpdateComment(Comment newComment)
+        {
+            using (ModelDesignContainer ctx = new ModelDesignContainer())
+            {
+                Comment oldComment = ctx.Comments.Find(newComment.commentId);
+                // Deoarece parametrul este un Comment ar trebui verificata fiecare
+                // proprietate din newComment daca are valoare atribuita si
+                // daca valoarea este diferita de cea din bd.
+                // Acest lucru il fac numai la modificarea asocierii.
+                if (newComment.text != null)
+                    oldComment.text = newComment.text;
+                if ((oldComment.postPostId != newComment.postPostId)
+               && (newComment.postPostId != 0))
+                {
+                    oldComment.postPostId = newComment.postPostId;
+                }
+                ctx.SaveChanges();
+                return oldComment;
+            }
+        }
+        public Comment GetCommentById(int id)
+        {
+            using (ModelDesignContainer ctx = new ModelDesignContainer())
+            {
+                var items = from c in ctx.Comments where (c.CommentId == id) select c;
+                return items.Include(p => p.Post).SingleOrDefault();
+            }
+        }
+    }
+}
--- Laboratoare/WCF si EF. Exemplu cu Post si Comment. - lab6/WFandEFLab/WFandEFLab/API/Post.cs
+++ Laboratoare/WCF si EF. Exemplu cu Post si Comment. - lab6/WFandEFLab/WFandEFLab/API/Post.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
+using WFandEFLab;
 
-namespace WFandEFLab.API
+namespace PostComment
 {
-    class Post
+    public partial class Post
     {
+        private int postId;
+
+        public bool AddPost()
+        {
+            using (ModelDesignContainer ctx = new ModelDesignContainer())
+            {
+                bool bResult = false;
+                if (this.postId == 0)
+                {
+                    var it = ctx.Entry<Post>(this).State = EntityState.Added;
+                    ctx.SaveChanges();
+                    bResult = true;
+                }
+                return bResult;
+            }
+        }
+        public Post UpdatePost(Post newPost)
+        {
+            using (ModelDesignContainer ctx = new ModelDesignContainer())
+            {
+                Post oldPost = ctx.Posts.Find(newPost.postId);
+                if (oldPost == null) // nu exista in bd
+                {
+                    return null;
+                }
+                oldPost.description = newPost.description;
+                oldPost.domain = newPost.domain;
+                oldPost.date = newPost.date;
+                ctx.SaveChanges();
+                return oldPost;
+            }
+        }
+        public int DeletePost(int id)
+        {
+            using (ModelDesignContainer ctx = new ModelDesignContainer())
+            {
+                return ctx.Database.ExecuteSqlCommand("Delete From Post where postid =
+               @p0", id);
+            }
+        }
+        public Post GetPostById(int id)
+        {
+            using (ModelDesignContainer ctx = new ModelDesignContainer())
+            {
+                var items = from p in ctx.Posts where (p.postId == id) select p;
+                if (items != null)
+                    return items.Include(c => c.Comments).SingleOrDefault();
+                return null; // trebuie verificat in apelant
+            }
+        }
+        public List<Post> GetAllPosts()
+        {
+            using (ModelDesignContainer ctx = new ModelDesignContainer())
+            {
+                return ctx.Posts.Include("Comments").ToList<Post>();
+                // Obligatoriu de verificat in apelant rezultatul primit.
+            }
+        }
     }
 }

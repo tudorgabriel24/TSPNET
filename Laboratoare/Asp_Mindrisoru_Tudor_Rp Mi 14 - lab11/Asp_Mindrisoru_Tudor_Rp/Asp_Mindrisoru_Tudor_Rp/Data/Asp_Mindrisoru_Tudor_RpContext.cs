1814de7a568173b6e26d343233b3e5c63eb3ae68
tudorgabriel24 <tudorgabriel1398@gmail.com>
2020-05-06 15:49:36 +0300

    add lab 11

---
+++ Laboratoare/Asp_Mindrisoru_Tudor_Rp Mi 14 - lab11/Asp_Mindrisoru_Tudor_Rp/Asp_Mindrisoru_Tudor_Rp/Data/Asp_Mindrisoru_Tudor_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Mindrisoru_Tudor_Rp.Models;
+
+namespace Asp_Mindrisoru_Tudor_Rp.Data
+{
+    public class Asp_Mindrisoru_Tudor_RpContext : DbContext
+    {
+        public Asp_Mindrisoru_Tudor_RpContext (DbContextOptions<Asp_Mindrisoru_Tudor_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Mindrisoru_Tudor_Rp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Laboratoare/Asp_Mindrisoru_Tudor_Rp Mi 14 - lab11/Asp_Mindrisoru_Tudor_Rp/Asp_Mindrisoru_Tudor_Rp/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Asp_Mindrisoru_Tudor_Rp.Data;
+using Asp_Mindrisoru_Tudor_Rp.Models;
+
+namespace Asp_Mindrisoru_Tudor_Rp
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Asp_Mindrisoru_Tudor_Rp.Data.Asp_Mindrisoru_Tudor_RpContext _context;
+
+        public IndexModel(Asp_Mindrisoru_Tudor_Rp.Data.Asp_Mindrisoru_Tudor_RpContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}

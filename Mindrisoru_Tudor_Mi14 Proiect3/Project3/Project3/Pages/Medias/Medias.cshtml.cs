00a992cd77cebe955eb23e2fa5f0280aabeaf966
tudorgabriel24 <tudorgabriel1398@gmail.com>
2020-05-26 16:27:49 +0300

    modify proiect3

---
+++ Mindrisoru_Tudor_Mi14 Proiect3/Project3/Project3/Pages/Medias/Medias.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project3.Pages.Medias
+{
+    public class MediasModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
